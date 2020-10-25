fc6c2ca4edf234067334e93d111653fadc153fef
ibrahimyaacob92 <ibrahimyaacob92@yahoo.com>
2020-10-25 16:42:43 +0800

    Update

--- script.js
+++ script.js
@@ -21,3 +21,26 @@ setInterval(()=>{
     glitch.innerText =currentVal
     },
 100)
+
+async function readJson(url){
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+// render the stacks
+var stackDiv = document.getElementById("stack-render")
+readJson('./data/mystack.json').then(data =>{
+    console.log(data)
+    let stackList = Object.keys(data)
+    for (let stack of stackList){
+        stackDiv.innerHTML += `<h4>${stack}</h4>`
+        let caption = data[stack].caption
+        stackDiv.innerHTML += `<p class="caption">${caption}</p>`
+        let modules = data[stack].modules
+        
+        for (let module of modules){
+            stackDiv.innerHTML += `<sl-tag type="primary" size="small">${module}</sl-tag>`
+        }
+    }
+})
